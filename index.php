1e8199bc65dcbd38d5dfd1c44030d3c39bdf2299
Martin Tomczak <martin.tomczak81@gmail.com>
2017-01-17 17:09:39 +0100

    Index.php File

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martintomczak
+ * Date: 16.01.17
+ * Time: 11:19
+ */
+
+require "framework/core/MailingFramework.php";
+
+
+Framework\Core\MailingFramework::run();
